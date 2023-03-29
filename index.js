bb59f62c900cf4d77dad7a578c24ff7869a5fceb
Anis <anis000romzi@gmail.com>
2023-03-29 20:39:02 +0700

    Optimize code

--- index.js
+++ index.js
@@ -6,45 +6,36 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
-app.get("/api/:date?", (req, res) => {
-  let unix;
-  let utc;
+app.get('/api/:date?', (req, res) => {
+  let unix = new Date(req.params.date || Date.now()).getTime();
+  let utc = new Date(req.params.date || Date.now()).toUTCString();
 
-  if (req.params.date) {
-    unix = new Date(req.params.date).getTime();
-    if (Number(req.params.date)) {
-      unix = Number(req.params.date);
-      utc = new Date(Number(req.params.date)).toUTCString();
-    } else {
-      utc = new Date(req.params.date).toUTCString();
-    }
-
-    if (utc === 'Invalid Date') {
-      res.json({ error: 'Invalid Date' });
-    } else {
-      res.json({ unix, utc });
-    }
-  } else {
-    res.json({ unix: new Date().getTime(), utc: new Date().toUTCString() });
+  if (Number(req.params.date)) {
+    unix = Number(req.params.date);
+    utc = new Date(Number(req.params.date)).toUTCString();
   }
+  if (utc === 'Invalid Date') {
+    res.json({ error: 'Invalid Date' });
+  }
+
+  res.json({ unix, utc });
 });
 
 // listen for requests :)
